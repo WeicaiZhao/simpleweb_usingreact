8d9cf2aa0966bda9fa06dbfab8aef5ee2c78928e
WeicaiZhao <wz1842@mun.ca>
2020-11-25 16:48:05 -0330

    delete no use method

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import logoPic from '../../statics/web-logo.png'
+// import logoPic from '../../statics/web-logo.png'
 
 export const HeaderWrapper = styled.div`
     position: relative;
@@ -14,7 +14,6 @@ export const Logo = styled.a.attrs({ href: '/'})`
     display: block;
     width: 100px
     height: 56px;
-    background: url(${logoPic});
     background-size: contain;
     
 `;
