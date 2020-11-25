4343a96608b92c6fca49120c90bbfb2d07be62ad
WeicaiZhao <wz1842@mun.ca>
2020-11-25 10:41:32 -0330

    set Styled-component with Reset-css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Fragment, Component} from 'react';
+import Header from "./common/header/index";
+import GlobalStyle from "./style";
+
+class App extends Component{
+    render() {
+        return(
+            <div>
+                <Fragment>
+                    <GlobalStyle/>
+                    <Header/>
+                </Fragment>
+            </div>
+        )
+    }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <App />,
+
   document.getElementById('root')
 );
 
